fb2f3a70276f01584412eeca9ef93623c58c79bc
yonezawa keita <e195731@ie.u-ryukyu.ac.jp>
2019-12-23 12:11:16 +0900

    1st commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e195731/Enemy.java
@@ -0,0 +1,24 @@
+package jp.ac.uryukyu.ie.e195731;
+
+/**
+ * 敵クラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+public class Enemy extends LivingThing {
+    public Enemy(String name,int maximumHP, int attack){
+        super(name,maximumHP,attack);
+    }
+
+    @Override
+    public void wounded(int damage) {
+        super.wounded(damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+}
